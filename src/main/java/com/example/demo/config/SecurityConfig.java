ffc389043fa2e0820a23f0a386164523d8b0dadc
gagreen <minsang6088@naver.com>
2025-11-28 12:01:40 +0900

    init

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {"/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        //@PreAuthorization 사용 -> 모든 경로에 대한 인증처리는 Pass
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+}
