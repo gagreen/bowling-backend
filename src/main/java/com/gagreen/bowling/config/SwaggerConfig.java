e4d0cfa308c7e7fbe0c9e4b8a764bf42ff04a656
gagreen <minsang6088@naver.com>
2025-12-11 17:11:18 +0900

    swagger ui 권한 추가

--- src/main/java/com/gagreen/bowling/config/SwaggerConfig.java
+++ src/main/java/com/gagreen/bowling/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.gagreen.bowling.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +14,22 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
 
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+
         return new OpenAPI()
                 .info(new Info()
                         .title("볼링 API")
                         .description("볼링장 관리 시스템 API 문서")
-                        .version("v1.0"));
+                        .version("v1.0"))
+                .addSecurityItem(securityRequirement)  // Security Requirement 추가
+                .schemaRequirement("BearerAuth", securityScheme);
     }
 }
